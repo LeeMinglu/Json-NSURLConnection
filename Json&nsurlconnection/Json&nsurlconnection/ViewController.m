a12a8edb35ff7f9e45837f235f4368166ae11f0e
LeeMinglu <402007353@qq.com>
2015-12-06 20:06:28 +0800

    解析数据NSlog中显示

--- Json&nsurlconnection/Json&nsurlconnection/ViewController.m
+++ Json&nsurlconnection/Json&nsurlconnection/ViewController.m
@@ -28,7 +28,19 @@
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
         NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
         
+        
+        // JSON - 将`二进制数据`转换成`字典` 数据解析，`反序列化`
+        /**
+         NSJSONReadingMutableContainers = (1UL << 0),   容器可变
+         NSJSONReadingMutableLeaves = (1UL << 1),       叶子可变
+         NSJSONReadingAllowFragments(碎片) = (1UL << 2)  顶级节点，可以不是数组或者字典
+         
+         直接做字典转模型，不关心节点变化，位移枚举传入0，效率最高！
+         */
+
         NSLog(@"%@",dict);
+        
+        NSLog(@"%@", dict[@"weatherinfo"][@"city"]);
     }];
 }
 
